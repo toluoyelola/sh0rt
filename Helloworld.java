86b324edfde1c5ae5f24ac98a412a0a02d816e5f
Tolu oyelola <toluoyelolzy@gmail.com>
2024-10-06 22:26:20 +0200

    import scanner, ask for input

---
+++ Helloworld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("What language do you speak? ");
+        String language = scanner.nextLine();
+
+
+    }
+}
