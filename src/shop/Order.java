f836735dffc75bc5ac19488a1eba7a355880535c
Tolu oyelola <toluoyelolzy@gmail.com>
2024-11-05 19:17:05 +0100

    remove commit

---
+++ src/shop/Order.java
@@ -0,0 +1,21 @@
+package shop;
+
+public class Order {
+
+    private int user_id;
+    private String contact;
+    private String orderStatus;
+    private int order_id;
+
+
+    public void createOrder() {
+
+    }
+
+    public void cancelOrder() {
+
+    }
+
+}
+
+
