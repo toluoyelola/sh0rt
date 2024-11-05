6fe8bc69bd5415eb167a27cd425559c33144e65e
Tolu oyelola <toluoyelolzy@gmail.com>
2024-11-05 19:01:55 +0100

    new classes for project

---
+++ src/shop/Products.java
@@ -0,0 +1,19 @@
+package shop;
+
+public class Products {
+
+    private String name;
+    private String description;
+    private String category;
+    private int product_id;
+    private double price;
+
+    public double dicount(){
+        return 1;
+    }
+
+    public void listCategory(){
+
+    }
+
+}
